61877af41f33b3aabdd76dd61289b02d6e54f94c
Derrick W. Turk <dwt@terminusdatascience.com>
2020-05-08 14:25:44 -0500

    first attempt - it's not UTF-8

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::Read,
+    str,
+};
+
+use zip::ZipArchive;
+
+const BUF_SIZE: usize = 4096; // 4kb at once
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = env::args().nth(1).ok_or("no filename provided")?;
+    let zipfile = File::open(path)?;
+    let mut zip = ZipArchive::new(zipfile)?;
+
+    if zip.len() != 1 {
+        Err("expected one file in zip archive")?;
+    }
+
+    let mut xmlfile = zip.by_index(0)?;
+    println!("file is {}, size {} bytes", xmlfile.name(), xmlfile.size());
+
+    let mut buf = [0u8; BUF_SIZE];
+    loop {
+        if xmlfile.read(&mut buf[..])? == 0 {
+            break;
+        }
+
+        println!("read chunk: {}", str::from_utf8(&buf[..])?);
+    }
+
+    Ok(())
+}
